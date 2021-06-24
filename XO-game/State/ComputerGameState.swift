8e7eca6bff4b388de1f612241b19b103d1c2f469
ilya rudenko <webilya29@gmail.com>
2021-06-24 12:51:37 +0300

    add new strategy computer vs user

---
+++ XO-game/State/ComputerGameState.swift
@@ -0,0 +1,21 @@
+//
+//  ComputerGameState.swift
+//  XO-game
+//
+//  Created by Ilya on 24.06.2021.
+//  Copyright © 2021 plasmon. All rights reserved.
+//
+
+import Foundation
+
+class ComputerGameState: GameState {
+    var isMoveCompleted: Bool = false
+    
+    func addSign(at position: GameboardPosition) {
+            
+    }
+    
+    func begin() {
+        
+    }
+}
